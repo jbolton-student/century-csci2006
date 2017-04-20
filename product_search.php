569502569a496c3b0230d958f17d6156c25c1e11
Jake Bolton <ninmonkeys@gmail.com>
2017-04-20 08:23:22 -0500

    Added Bruce's project_search code

--- product_search.php
+++ product_search.php
@@ -1,21 +1,63 @@
 <?php
-session_start();
+    session_start();                // TO-DO:  USE THIS INFO, SOMEHOW/SOMEWAY!!!
 
-require_once('common.php');
-require_once('db.php');
+    require_once('db.php');                                 // provides DB conn functions
+    // require_once('products.class.php');              // PROBABLY WON'T NEED THIS?!?
+
+
+    function listProducts($pdo, $searchValue) {
+    // using classes
+    $sql = "select * from products where name like '%" . $searchValue . "%'";
+    $statement = $pdo->prepare($sql);
+    $statement->execute();
+
+    $data = $statement->fetchAll();
+     $hitCount = $statement->rowCount();
+
+     if ($hitCount == 0 ) {
+        echo("<h3 class=\"loginFail\">No products have a name matching the search value!</h3><br />");
+    }
+     else {
+         // present data in a table
+         echo("<table><tr><th>Product Name</th><th>Price</th><th>Description</th><th>Image</th></tr>");
+         echo("<caption> $hitCount item(s) found: </caption>");
+         foreach($data as $i => $row) {
+             echo("<tr><td>".$row[2]."</td><td>".$row[1]."</td><td>".$row[4]."</td><td>");
+             // use line below if DB links to external image; following line links to local file in /images/
+             echo("<img src=\"$row[3]\" alt=\"$row[4]\" height=\"160\" width=\"160\"></td></tr>");
+             //echo("<img src=\"/project/images/$row[3]\" alt=\"$row[4]\" height=\"160\" width=\"160\"></td></tr>");
+         }
+         echo("</table>");
+
+        }
+    }
 
 ?>
 
-<html><body>
-<h1>Home</h1>
-<p>user: <b>
-<?php
-    echo getUsername();
-?>
-</b></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+    <title>Product Search</title>
+    <!-- Custom styles for this template -->
+   <link href="css/project_search.css" rel="stylesheet">
+</head>
 
-<p>Form here to search</p>
-<p>results here. See Products.test.php's listProducts() but modify function to compare search with form's input</p>
-<p>Button for "add to Cart". See: Cart.test.php</p>
+<body>
+    <h2>Product Search</h2>
+    <hr />
+    <form role="form" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+        <h3>Enter a product name or part of a name:</h3>
+        <input type="text" name="searchStr" />
+        <button type="submit">Search</button>
+        <br />
+        <?php
+            if ( ! empty($_POST['searchStr']) ) {
+                $pdo = DBConnect();                         // connect to the DB
+                listProducts($pdo, $_POST['searchStr']);
+            }
 
-</body></html>
+            $pdo = null;                                        // free-up DB resources
+        ?>
+</body>
+</html>
