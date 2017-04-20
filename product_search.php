cc77976f9d518dc19783323d408e7adb3760837a
Jake Bolton <ninmonkeys@gmail.com>
2017-04-20 08:34:15 -0500

    Updated query to prevent injection

--- product_search.php
+++ product_search.php
@@ -5,30 +5,38 @@
     // require_once('products.class.php');              // PROBABLY WON'T NEED THIS?!?
 
 
-    function listProducts($pdo, $searchValue) {
-    // using classes
-    $sql = "select * from products where name like '%" . $searchValue . "%'";
-    $statement = $pdo->prepare($sql);
-    $statement->execute();
-
-    $data = $statement->fetchAll();
-     $hitCount = $statement->rowCount();
-
-     if ($hitCount == 0 ) {
-        echo("<h3 class=\"loginFail\">No products have a name matching the search value!</h3><br />");
+    function getSearchTerm() {
+        if(isset($_POST['searchStr']))
+            return $_POST['searchStr'];
+        else
+            return "";
     }
-     else {
-         // present data in a table
-         echo("<table><tr><th>Product Name</th><th>Price</th><th>Description</th><th>Image</th></tr>");
-         echo("<caption> $hitCount item(s) found: </caption>");
-         foreach($data as $i => $row) {
-             echo("<tr><td>".$row[2]."</td><td>".$row[1]."</td><td>".$row[4]."</td><td>");
-             // use line below if DB links to external image; following line links to local file in /images/
-             echo("<img src=\"$row[3]\" alt=\"$row[4]\" height=\"160\" width=\"160\"></td></tr>");
-             //echo("<img src=\"/project/images/$row[3]\" alt=\"$row[4]\" height=\"160\" width=\"160\"></td></tr>");
-         }
-         echo("</table>");
 
+    function listProducts($pdo, $searchValue) {
+        // using classes
+        $sql = "select * from products where name like :searchValue";
+        $searchValue = "%" . $searchValue . "%";
+        $statement = $pdo->prepare($sql);
+        $statement->bindValue(":searchValue", $searchValue);
+        $statement->execute();
+
+        $data = $statement->fetchAll();
+        $hitCount = $statement->rowCount();
+
+        if ($hitCount == 0 ) {
+            echo("<h3 class=\"loginFail\">No products have a name matching the search value!</h3><br />");
+        } else {
+            // present data in a table
+            echo("<table><tr><th>Product Name</th><th>Price</th><th>Description</th><th>Image</th></tr>");
+            echo("<caption> $hitCount item(s) found: </caption>");
+
+            foreach($data as $i => $row) {
+                echo("<tr><td>".$row[2]."</td><td>".$row[1]."</td><td>".$row[4]."</td><td>");
+                // use line below if DB links to external image; following line links to local file in /images/
+                echo("<img src=\"$row[3]\" alt=\"$row[4]\" height=\"160\" width=\"160\"></td></tr>");
+                //echo("<img src=\"/project/images/$row[3]\" alt=\"$row[4]\" height=\"160\" width=\"160\"></td></tr>");
+            }
+            echo("</table>");
         }
     }
 
@@ -48,7 +56,7 @@
     <hr />
     <form role="form" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
         <h3>Enter a product name or part of a name:</h3>
-        <input type="text" name="searchStr" />
+        <input type="text" name="searchStr" value="<?php echo getSearchTerm(); ?>"/>
         <button type="submit">Search</button>
         <br />
         <?php
