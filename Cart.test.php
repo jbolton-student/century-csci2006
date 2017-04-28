8e8e4f3f902f8e8e3af374c7c35b6f9a202be1b1
Jake Bolton <ninmonkeys@gmail.com>
2017-04-28 14:58:02 -0500

    More Cart() work

--- Cart.test.php
+++ Cart.test.php
@@ -4,65 +4,30 @@ session_start();
 require_once('Products.class.php');
 require_once('Cart.class.php');
 
-$cart;
+// $cart;
 
-// todo: debug remove
+// debug: enable to always delete cart for testing
 if(isset($_SESSION['Cart'])) {
     unset($_SESSION["Cart"]);
 }
 
-if(!isset($_SESSION['Cart'])) {
-    $_SESSION['Cart'] = new Cart();
-}
+// if non-existing, create instance.
+initCart();
 
-$cart = $_SESSION['Cart'];
+$cart = getCart();
 $product = getProductByID(1);
-// echo $product;
+
+// can add either using Product() instance
+// or by the ID number alone.
 $cart->addItem($product);
+$cart->addItem(getProductByID(2));
+$cart->addItem(getProductByID(2));
 $cart->addItem(getProductByID(2));
 
 echo("<h1>listing</h1>");
 $cart->printCart();
 
 $_SESSION['Cart'] = $cart; // maybe redundant;
-
-
-//update shopping cart
-// $_SESSION['Cart'] = $cart;
-
-/* from class:
-
-$cart = $_SESSION['shoppingCart'];
-$item1 = new Item("iphone" , "smart phone", 799, 2);
-$cart->addItem($item1, "iphone");
-echo "iphone added successfully...<br>";
-
-$item2 = new Item("tv" , "smart tv",1229, 1);
-    $cart->addItem($item2, "tv");
-    echo "tv added successfully...<br>";
-
-//update shopping cart
-$_SESSION['shoppingCart'] = $cart;
-
-*/
-
-// function getCart() {}
-
-// function printCart() {
-//     echo "<h1>cart items:</h1>";
-
-//     if (isset($_SESSION['Cart'])) {
-//         $cart = $_SESSION['Cart'];
-
-//         $items = $cart->getItems();
-
-//         foreach ($items as $item) {
-//             echo $item->__toString() . "<br>";
-//         }
-
-//         echo "Total: " . $cart->getSubtotal() . " <br>";
-//     }
-// }
 
 ?>
 
--- cart.class.php
+++ cart.class.php
@@ -1,75 +1,91 @@
 <?php
+
+// ensures session instance exists
+function initCart() {
+    if(!isset($_SESSION['Cart'])) {
+        $_SESSION['Cart'] = new Cart();
+    }
+}
+
+function getCart() {
+    return $_SESSION['Cart'];
+}
+
 class Cart implements Countable {
-    private $items = Array();
+        private $items = Array();
 
-    // is the shopping cart empty?
-    public function isEmpty() {
-        return (empty($this->items));
-    }
-
-    // add ShoppingCartItem to cart
-    public function addItem($id) {
-        // $this->items[$key] = $id;
-        array_push($this->items, $id);
-    }
-
-    // remove an item from the cart
-    public function removeItem($id) {
-        if (isset($this->items[$id])) {
-            unset($this->items[$id]);
+        // is the shopping cart empty?
+        public function isEmpty() {
+            return (empty($this->items));
         }
-    }
 
-    // update the quantity of an item in the cart
-    // public function updateItem($id, $quantity) {
-    //     if (isset($this->items[$id])) {
-    //         if ($quantity == 0) {
-    //             $this->removeItem($id);
-    //         } else {
-    //             $item = $this->items[$id];
-    //             $item->setQuantity($quantity);
-    //             $this->items[$id] = $item;
-    //         }
-    //     }
-
-    // }
-
-    // return the count of the items in the cart
-    public function count() {
-        return count($this->items);
-    }
-
-    public function getItems() {
-        return $this->items;
-    }
-
-    // return subtotal of all items in the cart
-    public function getSubtotal() {
-        $subtotal = 0.0;
-        // die($this->count());
-        foreach($this->items as $item) {
-            $subtotal += $item->getCost();
+        // add ShoppingCartItem to cart
+        public function addItem($product) {
+            // $this->items[$key] = $product;
+            array_push($this->items, $product);
         }
-        return $subtotal;
-    }
 
-    public function __toString() {
-        $message = '';
-        foreach($this->items as $item) {
-            // $message += $item.toString();
-            $message += (string)$item;
+        // remove an item from the cart
+        public function removeItem($id) {
+            // todo test this:
+            foreach($this->items as $item) {
+                if (isset($this->items[$id])
+                    && $this->items.getID() == $id)
+                {
+                        unset($this->items[$id]);
+                }
+            }
         }
-        return $message;
-    }
 
-function printCart() {
-    echo "<h1>cart items:</h1>";
-    foreach ($this->items as $item) {
-        echo $item->__toString() . "<br>";
-    }
+        // update the quantity of an item in the cart
+        // public function updateItem($id, $quantity) {
+        //     if (isset($this->items[$id])) {
+        //         if ($quantity == 0) {
+        //             $this->removeItem($id);
+        //         } else {
+        //             $item = $this->items[$id];
+        //             $item->setQuantity($quantity);
+        //             $this->items[$id] = $item;
+        //         }
+        //     }
 
-    echo "Total: " . $this->getSubtotal() . " <br>";
+        // }
+
+        // return the count of the items in the cart
+        public function count() {
+            return count($this->items);
+        }
+
+        public function getItems() {
+            return $this->items;
+        }
+
+        // return subtotal of all items in the cart
+        public function getSubtotal() {
+            $subtotal = 0.0;
+            foreach($this->items as $item) {
+                $subtotal += $item->getCost();
+            }
+            return $subtotal;
+        }
+
+        public function __toString() {
+            $message = '';
+            foreach($this->items as $item) {
+                // $message += $item.toString();
+                $message += (string)$item;
+            }
+            return $message;
+        }
+
+    function printCart() {
+        echo "<h1>cart items:</h1>";
+        foreach ($this->items as $item) {
+            echo $item->__toString() . "<br>";
+        }
+
+        echo "Total: " . $this->getSubtotal() . " <br>";
+    }
 }
 
-}
 ?>
